09bd9dbed326389e906b5aa0bacd2378c7d61c95
Rodrigo Tozatti <rodrigo.tozatti@hotmail.com>
2022-07-09 20:01:32 -0300

    utilizando faker

--- cypress/e2e/pre-cadastro.spec.cy.js
+++ cypress/e2e/pre-cadastro.spec.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-var faker = require('faker');
+import { Faker, faker } from '@faker-js/faker';
 
 describe('funcionalidade pre cadasteo', () => {
 
@@ -10,10 +10,15 @@ describe('funcionalidade pre cadasteo', () => {
 
   it('deve completar o pre cadastro com sucesso', () => {
     cy.get('#reg_email').type(faker.internet.email())
-    cy.get('#reg_password').type('!testeebac01$$')
+    cy.get('#reg_password').type(faker.internet.password())
     cy.get(':nth-child(4) > .button').click()
 
     cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+    cy.get('#account_first_name').type(faker.name.firstName())
+    cy.get('#account_last_name').type(faker.name.lastName())
+    cy.get('.woocommerce-Button').click()
+
+    cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso.')
   });
   
 });
