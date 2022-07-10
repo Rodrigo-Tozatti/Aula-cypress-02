a95ac05543276782f5e0521180325a703e17d293
Rodrigo Tozatti <rodrigo.tozatti@hotmail.com>
2022-07-10 20:33:52 -0300

    listas e variaveis

--- cypress/e2e/pre-cadastro.spec.cy.js
+++ cypress/e2e/pre-cadastro.spec.cy.js
@@ -9,13 +9,18 @@ describe('funcionalidade pre cadasteo', () => {
   });
 
   it('deve completar o pre cadastro com sucesso', () => {
-    cy.get('#reg_email').type(faker.internet.email())
-    cy.get('#reg_password').type(faker.internet.password())
+    var emailFaker = faker.internet.email
+    var password = faker.internet.password
+    var primeiroNome = faker.name.firstName
+    var segundoNome = faker.name.lastName
+
+    cy.get('#reg_email').type(emailFaker())
+    cy.get('#reg_password').type(password())
     cy.get(':nth-child(4) > .button').click()
 
     cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-    cy.get('#account_first_name').type(faker.name.firstName())
-    cy.get('#account_last_name').type(faker.name.lastName())
+    cy.get('#account_first_name').type(primeiroNome())
+    cy.get('#account_last_name').type(segundoNome())
     cy.get('.woocommerce-Button').click()
 
     cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso.')
--- cypress/e2e/produtos.spec.cy.js
+++ cypress/e2e/produtos.spec.cy.js
@@ -2,7 +2,7 @@
 
 describe('Funcionalidade Página de Produtos', () => {
 
-  before(() => {
+  beforeEach(() => {
     cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
   });
 
@@ -11,12 +11,16 @@ describe('Funcionalidade Página de Produtos', () => {
   });
 
   it.only('deve adicionar um produto ao carrinho', () => {
+    var quantidade = 4
+
     cy.get('.product-block').eq(3).click()
     cy.get('.button-variable-item-M').click()
     cy.get('.button-variable-item-Blue').click()
-    cy.get('.input-text').clear().type(3)
+    cy.get('.input-text').clear().type(quantidade)
     cy.get('.single_add_to_cart_button').click()
 
+    cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+    cy.get('.woocommerce-message').should('contain', quantidade +' × “Ajax Full-Zip Sweatshirt” foram adicionados no seu carrinho.')
   });
 
 });
