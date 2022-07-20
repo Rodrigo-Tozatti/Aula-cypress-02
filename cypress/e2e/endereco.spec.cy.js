b35eb6638d0b13dc0eba13614257abcc282587b8
Rodrigo Tozatti <rodrigo.tozatti@hotmail.com>
2022-07-20 13:03:02 -0300

    Aula pages

--- cypress/e2e/endereco.spec.cy.js
+++ cypress/e2e/endereco.spec.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import Enderecopage from  '../support/Page-objects/Endereco.page'
 
 describe('Funcionalidade enderecos - faturamento e entrega', () => {
 
@@ -10,6 +11,9 @@ describe('Funcionalidade enderecos - faturamento e entrega', () => {
   });
 
   it('Deve fazer cadastro de faturamento com sucesso', () => {
+    Enderecopage.editarEnderecoFaturamento('Rodrigo', 'Rosseti', 'Fera_02', 'Brasil', 'Rua Jordão, 604', 'Lote 06, quarda 08', 'São Paulo', 'São Paulo', '87911-000', '(43) 98840-9982', 'rodrigo@rodrigo.com')
+
+    cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
 
   });
 });
